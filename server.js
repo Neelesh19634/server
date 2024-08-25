75fa4b205b6c23ba192c8ac7d1cff2baddffedd0
Neelesh19634 <143541329+Neelesh19634@users.noreply.github.com>
2024-08-25 21:03:58 +0530

    update

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ require("dotenv").config()
 const express=require('express')
 const app=express()
 const cors=require('cors')
-const connect=require('./db')
+
 const router=require('./auth-router')
 const path=require('path')
+const mongoose=require('mongoose')
 const option={
     origin:"http://localhost:3000",
     methods: "GET,POST,PUT,DELETE,PATCH,HEAD",
@@ -15,11 +16,21 @@ app.use(express.json());
 app.use("/public", express.static("public"));
 
 app.use("/",router)
+const connect = async () => {
+    try {
+    await mongoose.connect(process.env.URL);
+    console.log("connected to database");
+    } catch (error) {
+    console.log("error in connecting to database");
+    process.exit(0);
+    }
+    };
 
 
 
-
-    const PORT=process.env.PORT;
-    app.listen(PORT,()=>{
-        console.log(`server is running on port ${PORT}`)
-    })
+    connect().then(() => {
+        const PORT = process.env.PORT;
+        app.listen(PORT, () => {
+          console.log(`server is running on port ${PORT}`);
+        });
+      });
