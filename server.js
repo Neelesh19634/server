12ce1a5b53663d3b7eb7bc73f3ee007a66bb666b
Neelesh19634 <143541329+Neelesh19634@users.noreply.github.com>
2024-08-25 20:27:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ app.use("/",router)
 
 
 
-    const PORT=process.env.PORT;
-    app.listen(PORT,()=>{
-        console.log(`server is running on port ${PORT}`)
-    })
+connect().then(() => {
+    const PORT = process.env.PORT;
+    app.listen(PORT, () => {
+      console.log(`server is running on port ${PORT}`);
+    });
+  });
